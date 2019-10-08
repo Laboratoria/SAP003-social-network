8da1d3d10acc21af0d9ffb1bb05cb575ac94b7d2
rosi-souza <rosi.souza@live.com>
2019-10-08 15:52:17 -0300

    pegar valores dos inputs

--- src/components/input.js
+++ src/components/input.js
@@ -1,9 +1,8 @@
 function Input(props) {
   const template = `
-    <input class="input" 
+    <input class="${props.class}" 
       placeholder=${props.placeholder} 
       type=${props.type}  
-      id=${props.id}
     >
   `;
   return template;
--- src/pages/home.js
+++ src/pages/home.js
@@ -2,14 +2,23 @@ import Button from '../components/button.js';
 // import Card from '../components/card.js';
 import Input from '../components/input.js';
 
+
+function pegarInput() {
+  const email = document.querySelector('.js-email').value;
+  const senha = document.querySelector('.js-senha').value;
+  console.log(email, senha);
+}
+
 function Home() {
   const template = `
     <h1>Home Page</h1>
     <p>Esse é um exemplo 🍌</p>
-    ${Input({ placeholder: 'Email', type: 'email' })}
-    ${Input({ placeholder: 'Senha', type: 'password' })}
-    ${Button({ id: '🎉', title: 'Login' })}
-    `;
+    <form>
+      ${Input({ class: 'js-email', placeholder: 'Email', type: 'email' })}
+      ${Input({ class: 'js-senha', placeholder: 'Senha', type: 'password' })}
+      ${Button({ id: '🎉', title: 'Login', onClick: pegarInput })}
+    </form>
+  `;
   return template;
 }
 // ${Card({ texto: 'banana e peixinho' })}
