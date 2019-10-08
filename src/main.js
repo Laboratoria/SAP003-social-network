1146fb856c7d32171e4938ca609d81165b1fb0cb
aline-camargo <aline97camargo@gmail.com>
2019-10-07 21:54:32 -0300

    login email/google/facebook ok

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Home from './pages/home.js';
+import login from './pages/login.js';
 
 function init() {
   document.querySelector('main').innerHTML = Home();
+  Object.values(login).forEach( f => f() );
+  FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
+  });
 }
 
 window.addEventListener('load', init);
-
