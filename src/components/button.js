47e2e63dea9f058282fdd489ff09be7fb1d0fde4
KarineVerneque <karinemvs01@hotmail.com>
2019-10-08 17:38:00 -0300

    login changes

--- src/components/button.js
+++ src/components/button.js
@@ -1,24 +1,17 @@
+// import { callbac } from "util";
+
 function Button(props) {
   const template = `
-    <button class="primary-button" onclick="button.handleClick(event)" >${props.title}</button>
+    <button class="${props.class} primary-button" onclick="button.handleClick(event, ${props.onClick})" >${props.title}</button>
   `;
 
   return template;
 }
 
 window.button = {
-  handleClick: (event) => {
+  handleClick: (event, callback) => {
     event.preventDefault()
-    const email = document.querySelector('.input-email').value
-    const password = document.querySelector('.input-password').value
-    console.log(email);
-    console.log(password);
-    firebase.auth().signInWithEmailAndPassword(email, password).catch(function() {
-      // Handle Errors here.
-      //var errorCode = error.code;
-      //var errorMessage = error.message;
-      // ...
-    });
+    callback()
   },
 };
 
