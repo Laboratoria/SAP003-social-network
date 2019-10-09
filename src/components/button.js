7e0df49749b4909af31fa14f00c50ad3f097eb1d
rosi-souza <rosi.souza@live.com>
2019-10-09 14:32:55 -0300

    dados do cadastro no localstorage

--- src/components/button.js
+++ src/components/button.js
@@ -6,16 +6,14 @@ function Button(props) {
 
       ${props.title}
     </button>
- 
+  `;
   return template;
 }
 
 window.button = {
   handleClick: (event, callback) => {
     event.preventDefault();
-    const email = document.querySelector('.js-email').value;
-    const senha = document.querySelector('.js-senha').value;
-    console.log( email,senha);
+    callback();
   },
 };
 
