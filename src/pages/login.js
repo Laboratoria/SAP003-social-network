19dc31f7ade8541eaf4c0c4fd3065096c09d2d40
thamiresoliveiragomes <thamiresoliveira1004@gmail.com>
2019-10-09 17:54:07 -0300

    iniciando css

--- src/pages/login.js
+++ src/pages/login.js
@@ -2,25 +2,25 @@ import Button from '../components/button.js';
 import Input from '../components/input.js';
 
 function Login() {
-
-   function changePage() {
+  function changePage() {
     const email = document.querySelector('.js-email-input').value;
     const password = document.querySelector('.js-password-input').value;
-    firebase.auth().signInWithEmailAndPassword(email, password).then((certo) => {
-      console.log('certo')
+    firebase.auth().signInWithEmailAndPassword(email, password).then(() => {
+      console.log('certo');
       window.location.href = '#feed';
-    },(error) => {
-    // Handle Errors here.  
+    }, (error) => {
+    // Handle Errors here.
       const errorCode = error.code;
       const errorMessage = error.message;
-      console.log(errorMessage)
+      console.log(errorMessage);
+      console.log(errorCode);
     });
   }
-  
   const template = `
   <figure>
-  <img src="/imagens/figure1.jpg" alt="logo-viagem"/>
+    <img class="logo" src="imagens/figure1.jpg" alt="logo-viagem"/>
   </figure>
+
     <section class="box-login">
     <h1 class="title"> nomeee </h1>
     <h3 class="subtitle">Bem vindo, viajante!</h3>
@@ -28,7 +28,6 @@ function Login() {
         ${Input({ type: 'email', class: 'js-email-input', placeholder: 'email' })}
         ${Input({ type: 'password', class: 'js-password-input', placeholder: 'password' })}
         ${Button({ class: 'signIn', title: 'signIn', onclick: changePage })}
-        
       </form>
       <a href="#register">Create an Account</a>
       </section>
@@ -36,6 +35,5 @@ function Login() {
   window.location.href = '#login';
   return template;
 }
-
 
 export default Login;
