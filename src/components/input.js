cbdc1749dd9ad38b7df7333c761f36c1b833cb32
vanesnery <nery_van@hotmail.com>
2019-10-09 15:49:56 -0300

    Login simples

---
+++ src/components/input.js
@@ -0,0 +1,11 @@
+function Input(props) {
+  const template = `<input type= '${props.type}' 
+  placeholder= '${props.placeholder}'
+  class= '${props.class}'/>`
+    ;
+
+  return template;
+}
+
+
+export default Input;
