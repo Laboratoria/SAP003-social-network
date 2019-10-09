be9036278dbab273d1ca5112e2d725c76c11abd3
anacola <anac.cola@gmail.com>
2019-10-09 14:05:53 -0300

    Atualização de botão de registro

--- src/components/button.js
+++ src/components/button.js
@@ -1,20 +1,11 @@
 function Button(props) {
-  return `<button class="primary-button" onclick="button.handleClick(event, '${props.id}')" >${props.title}</button>`;
+  return `<button class="primary-button" onclick="button.handleClick(event, ${props.onClick})"> ${props.title}</button>`;
 }
 
 window.button = {
-  handleClick: (event) => {
+  handleClick: (event, callback) => {
     event.preventDefault();
-
-    const email = document.querySelector('#email').value;
-    const password = document.querySelector('#password').value;
-
-    firebase.auth().createUserWithEmailAndPassword(email, password).catch((error) => {
-      // Handle Errors here.
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      // ...
-    });
+    callback()  
   },
 };
 
