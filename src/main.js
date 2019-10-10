a561ddb7865cd5e393442e5708fb48f7b5b9bdc4
Camila <cm.maschion@gmail.com>
2019-10-10 16:41:55 -0300

    nao aguento mais o git

--- src/main.js
+++ src/main.js
@@ -1,7 +1,26 @@
 import Home from './pages/home.js';
+import Register from './pages/register.js';
+import Login from './pages/login.js';
+import Feed from './pages/feed.js';
 
-function init() {
-  document.querySelector('main').innerHTML = Home();
+const pages = {
+  home: Home(),
+  register: Register(),
+  login: Login(),
+  feed: Feed(),
+};
+
+function init(){
+  document.querySelector('main').innerHTML = Login();
+}
+window.addEventListener('load', init);
+
+window.addEventListener('hashchange', () => {
+  //const pageHash = location 
+})
+
+/*function init() {
+  document.querySelector('main').innerHTML = Login();
 }
 
-window.addEventListener('load', init);
+window.addEventListener('load', init);*/
---
+++ src/pages/login.js
@@ -0,0 +1,36 @@
+import Button from '../components/button.js';
+import Input from '../components/input.js';
+import AuthEmailPassButton from '../components/authentication.js';
+
+function createLogin() {
+  const email=document.querySelector('.js-email-input');
+  const password=document.querySelector('.js-password-input').value;
+
+  firebase.auth().createUserWithEmailAndPassword(email.value, password)
+    .then(function (email){
+      alert(`Bem vindo ` + email.value);
+      
+    })
+      .cath(function (error){
+        console.error(error.code);
+        console.error(error.message);
+        alert(`Falha ao cadastrar, verifique o erro no console`)
+      });
+}
+
+
+
+function Login() {
+  const template = `
+    <h1>Login</h1>
+    <form>
+    ${Input({ class:'js-email-input', placeholder:'E-mail', type:'email', })}
+    ${Input({ class:'js-password-input', placeholder:'Senha', type:'password', })}
+    ${Button({ id: '🎉', title: 'Log In', onClick: AuthEmailPassButton})}
+    ${Button({ id: '🎉', title: 'Criar Usuario', onClick: createLogin})}
+    </form>`;
+
+  return template;
+}
+
+export default Login;
