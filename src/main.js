66202fd518131c7b2cf0a21c55b54589b285ff69
aline-camargo <aline97camargo@gmail.com>
2019-10-10 17:43:30 -0300

    login google e rotas

--- src/main.js
+++ src/main.js
@@ -1,11 +1,18 @@
 import Register from './pages/register.js';
 import Login from "./pages/login.js"
+
 const init = () => {
+  location.hash = "";
   document.querySelector('main').innerHTML = Login();
-  // uiConfig();
 }
 
-window.addEventListener('load', init);
+const routes = () =>{
+  if(location.hash === '#register'){
+    document.querySelector('main').innerHTML = Register();
+  } else if (location.hash === "") {
+    document.querySelector('main').innerHTML = Login();
+  }
+}
 
 // import { initRouter } from "./route.js";
 // import { observer } from "./assets/js/auth.js";
@@ -13,3 +20,5 @@ window.addEventListener('load', init);
 // const init = () => {
 //   firebase.auth().onAuthStateChanged(e => initRouter());
 // };
+window.addEventListener('load', routes);
+window.addEventListener('hashchange', routes);
