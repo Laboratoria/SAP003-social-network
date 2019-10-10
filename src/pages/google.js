c162fa291281dd55bf7b607b91d1a751626ba314
vanesnery <nery_van@hotmail.com>
2019-10-10 16:19:40 -0300

    Login do google funcionado

---
+++ src/pages/google.js
@@ -0,0 +1,23 @@
+function Google (){
+var provider = new firebase.auth.GoogleAuthProvider();
+    // signIn(provider);
+firebase.auth().languageCode = 'pt';
+firebase.auth().signInWithPopup(provider).then(function(result) {
+  // This gives you a Google Access Token. You can use it to access the Google API.
+  var token = result.credential.accessToken;
+  // The signed-in user info.
+  var user = result.user;
+  // ...
+}).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // The email of the user's account used.
+  var email = error.email;
+  // The firebase.auth.AuthCredential type that was used.
+  var credential = error.credential;
+  // ...
+});
+}
+  window.location.href = '#singOut';
+export default Google;
