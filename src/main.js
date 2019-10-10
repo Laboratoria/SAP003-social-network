89a89950efeef51f83431c5bb49da510d2a97016
Jessica Patricia <jessicapatricia03@hotmail.com>
2019-10-10 15:04:25 -0300

    iniciando autenticação

--- src/main.js
+++ src/main.js
@@ -22,7 +22,47 @@ const hash = () => {
 		return cad();
 	}
 }
+//mudança de hash #
 
 window.addEventListener("load", init);
 
 window.addEventListener("hashchange", hash, false);
+
+
+
+
+let authEmailPassButton = document.getElementById("authEmailPassButton");
+let createUserButton = document.getElementById("createUserButton");
+ 
+let authGoogleButton = document.getElementById("authGoogleButton");
+let authFacebookButton = document.getElementById("authFacebookButton");
+
+let logOutButton = document.getElementById("logOutButton");
+
+//Inputs
+let emailInput = document.getElementById("emailInput");
+let passwordInput = document.getElementById("passwordInput");
+
+//Displays 
+let displayName = document.getElementById("displayName");
+
+createUserButton.addEventListener("click", function(){
+	firebase
+		.auth().createUserWithEmailAndPassword(emailInput.Value, passwordInput.value)
+		.then(function() {
+			alert("Bem vindo" + emailInput.value)
+		})
+		.catch(function (error) {
+			console.error(error.code);
+			console.error(error.message);
+			alert("Falha ao cadastrar, verifique o erro no console.")
+		})
+})
+
+firebase.auth().onAuthStateChanged(function(user) {
+	if (user) {
+	  // User is signed in.
+	} else {
+	  // No user is signed in.
+	}
+  });
