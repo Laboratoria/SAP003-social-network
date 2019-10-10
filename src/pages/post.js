610e876c2e185cd1058c4045edf70a0b60375295
JessikaFujimura <jessikafujimura@gmail.com>
2019-10-10 13:22:25 -0300

    hash funcionando e criando id no firestore

--- src/pages/post.js
+++ src/pages/post.js
@@ -1,6 +1,32 @@
+import Input from '../components/input.js';
+import Button from '../components/button.js';
+
 function Post() {
-  const template = 
-    console.log("OI");
+  const template = `
+  ${Input({
+      class: 'js-post',
+      placeholder: 'O que quer compartilhar?',
+      type: 'text',
+    })}
+  ${Button({
+    id: 'share',
+    title: 'Compartilhar',
+    onClick: SharePost,
+  })}
+  `;
   return template;
 }
+
+function SharePost() {
+  const docRef = firebase.firestore().collection('timeline');
+  const contPost = document.querySelector('.js-post').value;
+  docRef.add({
+    post: contPost
+  }).then(function() {
+    console.log('Post Salvo');
+  }).catch(function(error) {
+    console.log('O erro é: ', error);
+  })
+}
 export default Post;
+
