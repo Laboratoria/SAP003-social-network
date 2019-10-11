6960881f4dbf7c6004111f84464ccc07526a19a8
tiemimaeda <priscila.maeda@yahoo.com.br>
2019-10-11 16:26:12 -0300

    Função cadastro novo usuário

---
+++ src/components/input.js
@@ -0,0 +1,11 @@
+function Input(props) {
+  const template = `
+    <input
+    class="${props.class} primary-input"
+    placeholder="${props.placeholder}"
+    type="${props.type}" />
+`;
+  return template;
+}
+
+export default Input;
--- src/main.js
+++ src/main.js
@@ -1,7 +1,20 @@
-import Home from './pages/home.js';
+// import Home from './pages/home.js';
+import Register from './pages/register.js';
+import NewPage from './pages/newpage.js';
 
-function init() {
-  document.querySelector('main').innerHTML = Home();
+function pageRoute() {
+  const pageName = (window.location.hash) ? window.location.hash : '#register';
+  switch (pageName) {
+    case '#register':
+      document.querySelector('main').innerHTML = Register();
+      break;
+    case '#newpage':
+      document.querySelector('main').innerHTML = NewPage();
+      break;
+    default:
+      document.querySelector('main').innerHTML = Register();
+  }
 }
 
-window.addEventListener('load', init);
+window.addEventListener('load', pageRoute);
+window.addEventListener('hashchange', pageRoute, false);
