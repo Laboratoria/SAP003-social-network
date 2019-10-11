6d66c52ff638c4030197d8a8c60adeafc0bfda2c
katypaiva <katyfeitozaa@gmail.com>
2019-10-11 10:48:31 -0300

    função de logar com o google

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Login from './pages/login.js';
 import Register from './pages/register.js';
 
 function init() {
-  console.log(firebase.auth())
+
   document.querySelector('main').innerHTML = Login();
 }
 
