4471b5de8c45a4e8b576ecccf368e4eca844b32f
claudiakemi <claudiakemis@gmail.com>
2019-10-11 11:32:09 -0300

    css alterado

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-// import Button from '../components/button.js';
-//
-// function Home() {
-//   const template = `
-//     <h1>Login</h1>
-//     ${Button({ id: '🐠', title: 'Botão 🐠' })}
-//     ${Button({ id: '🎉', title: 'Botão 🎉' })}
-//     <p>Esse é um exemplo 🍌</p>
-//   `;
-//
-//   return template;
-// }
-//
-// export default Home;
-=======
 import Button from '../components/button.js';
 
 function Home() {
@@ -28,4 +12,3 @@ function Home() {
 
 window.location.href = '#authentication';
 export default Home;
->>>>>>> 406c9e5c3ceea29ecad43d0988a0e9d4047eabc7
