a9ab59d1e1f881c662dcd58f8554a47a8316393f
mokasousa <moka.sousa@gmail.com>
2019-10-14 13:41:49 -0300

    database info usuario e rota pro feed atualizada

--- src/main.js
+++ src/main.js
@@ -1,21 +1,38 @@
 import Login from './pages/login.js';
 import Signup from './pages/signup.js';
-import Feed from './pages/feed.js';
-//import isAuthenticated from './auth.js';
+import Signout from './pages/feed.js';
+import isAuthenticated from './auth.js';
 
-window.addEventListener('load', function() {
-  window.location = '#login';
-  locationHashChanged(); 
-});
+window.addEventListener('load', locationHashChanged);
 window.addEventListener("hashchange", locationHashChanged, false);
 
-
-function locationHashChanged() {
-  if (location.hash === '#login'){
-    document.querySelector('main').innerHTML = Login();
-  } else if (location.hash === '#feed') {
-    document.querySelector('main').innerHTML = Feed();
-  } else if (location.hash === '#signup') {
-    document.querySelector('main').innerHTML = Signup();
-  }
+function locationHashChanged(){
+ if (isAuthenticated()) {
+   if (location.hash === '#login') {
+     location.hash = '#feed';
+     document.querySelector('main').innerHTML = Signout();
+   }
+   else if (location.hash ==='#feed') {
+     document.querySelector('main').innerHTML = Signout();
+   }
+   else if(location.hash ==='#signup'){
+     document.querySelector('main').innerHTML = Signup();
+   }
+ }
+ else {
+   if (location.hash === '#login') {
+     document.querySelector('main').innerHTML = Login();
+   }
+   else if(location.hash ==='#signup'){
+     document.querySelector('main').innerHTML = Signup();
+   }
+ }
 }
+
+//#login página de login
+//#signup href do "cadastre-se"
+//#feed página do feed
+//Logout(), função que faz logout
+//Login()função que tem a tela do login
+//Signup() função da tela de cadastro
+//Signout()função do botão signout
--- src/pages/feed.js
+++ src/pages/feed.js
@@ -21,13 +21,26 @@ function userInfo() {
     <p>${user.email}</p>
     `;
     document.querySelector('.profile').innerHTML = username;
+  });  
+}
+
+function publishText() {
+  console.log('publicou');
+  const userText = document.getElementById('user-publication').value;
+  db.collection('publi').add({
+    text: userText,
   });
-  
-  
 }
 
 function Feed() {
-  const template = `<div class='profile'></div>${Button({ id: 'btn-log-out', onclick:logOut, title: 'Sair'})}`;
+  const template = `
+  <div class='profile'></div>
+  <form class='publications'>
+    <textarea id='user-publication'>
+    ${Button({ id: 'btn-publish', onclick:publishText, title: 'Publicar'})};
+  </form>
+  ${Button({ id: 'btn-log-out', onclick:logOut, title: 'Sair'})}
+  `;
   userInfo()
   return template;
 }
