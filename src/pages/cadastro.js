2ad58bbf7ab45198f9e0c319334d0667763fc2ba
evs <evoraleite@gmail.com>
2019-10-14 17:47:34 -0300

    working on firestore

--- src/pages/cadastro.js
+++ src/pages/cadastro.js
@@ -15,7 +15,20 @@ const cadastrar = (id, event) => {
   var errorMessage = error.message;
   // ...
   alert(errorMessage);
-	})	
+	})
+
+	const name = document.getElementById('name').value;
+
+
+//ESTOU AQUI
+	const user = {
+		name: name,
+		email: email
+		// bio:
+		// nascimento:
+	}
+
+	firebase.firestore().collection('users').add(user);
 }
 
 export const Cadastro = () => {
@@ -25,10 +38,13 @@ export const Cadastro = () => {
 		${Input({ id:"name",  placeholder:"Nome", type:"text"})}
 		${Input({ id:"mail", placeholder:"Email", type:"email"})}
 		${Input({ id:"pass", placeholder:"Senha", type:"password"})}
-		${Button({ class:'btn btn-send-sign-up', id:"cad", title: "Cadastrar", type:"submit", value:"submit", onclick: cadastrar})}</form>`;
+		${Button({ class:'btn btn-send-sign-up', id:"cad", title: "Cadastrar", type:"submit", value:"submit", onclick: cadastrar})}</form>
+		${Button({ class:'btn-go-home', id:"go-home", type:"button", title:"Back Home", onclick: goHome})}`;
 
 	return template;
 }
 
-
+const goHome = () => {
+	window.location.hash = 'home';
+}
 
