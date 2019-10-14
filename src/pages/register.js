147ef690f9ca0cdd6b515b1640d64682b7c7525f
Fabiana Fonseca <fsf.fabiana8@gmail.com>
2019-10-14 13:51:45 -0300

    mobile firt

--- src/pages/register.js
+++ src/pages/register.js
@@ -1,8 +1,6 @@
 import Button from '../components/button.js';
 import Input from '../components/input.js';
 
-
-
 function register(){
     const template = `
     ${Button({id:'voltar', title: 'Voltar', onClick: goToLogin})}    
@@ -22,8 +20,34 @@ function register(){
   }
   function buttonRegister() {
     const email= document.querySelector('.js-email-input').value;
-    const password= document.querySelector('js-password-input').value;
-    }
+    const password= document.querySelector('.js-password-input').value;
+    firebase.auth().createUserWithEmailAndPassword(email, password).then(() => {
+      firebase.auth().onAuthStateChange(function(user){
+        if(user) {
+          console.log('funciona');
+        } else {
+          alert('não funciona')
+        }
+      })
+    }).catch(function(user) {
+      // Handle Errors here.
+      // var errorCode = error.code;
+      // var errorMessage = error.message;
+      // ...
+    });
+    // firebase.auth().createUser({
+    //   email: 'user@example.com',
+    //   emailVerified: false,
+    //   password: 'secretPassword'
+    // })
+    // .then(function(userRecord) {
+    //   console.log('logado', userRecord.id)
+    // })
+    // .catch(function(error){
+    //   console.log('erro', error)
+    // });
+  }
   
+ 
   
   export default register;
