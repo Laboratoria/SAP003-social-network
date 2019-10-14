734aff41b0fbe265e2559f3ed0e9a3c89a87c0ff
lefsampaio <leticia.cosmetic2011@gmail.com>
2019-10-14 16:08:54 -0300

    postar no feed

--- src/pages/feed.js
+++ src/pages/feed.js
@@ -1,43 +1,57 @@
 import Button from '../components/button.js';
 
-;
+
 const location = () => {
-    location.hash = '';
+  location.hash = '';
 }
 
-const logout= ()=>{
-  firebase.auth().signOut().then(function() {
-    location.hash=''
-  }).catch(function(error) {
+const logout = () => {
+  firebase.auth().signOut().then(function () {
+    location.hash = ''
+  }).catch(function (error) {
     // An error happened.
   });
 }
+
 const newPost = () => {
   const db = firebase.firestore();
-  const post={
+  const post = {
+    //id
     text: document.querySelector('.add-post').value,
-    date: new Date().toTimeString().substring(0,5)
+    day: new Date().toLocaleDateString(),
+    hour: new Date().toTimeString().substring(0, 5)
   }
- db.collection('posts').add(post);
- document.querySelector('.posts').innerHTML +=
- `<li> ${post.text} | ${post.date}</li>`
+  db.collection('posts').add(post);
+}
+
+const showPosts = () => {
+  const db = firebase.firestore();
+  db.collection('posts').orderBy('day').orderBy('hour','asc').get().then((snapshot) => {
+    snapshot.docs.forEach(doc => {
+      document.querySelector('.posts').innerHTML +=
+        `<p> ${doc.data().text} |${doc.data().day}| ${doc.data().hour}</p>`
+
+    });
+  });
+
 }
 
 function Feed() {
-    const template = `
+  const template = `
     <section class="container">
       <section class="container">
       <textarea class="add-post" placeholder="O que você está ouvindo?"></textarea>
       ${Button({ type: 'button', title: 'Postar', class: 'primary-button', onClick: newPost })}
-        <ul class="posts">  </ul>
+        <div class="posts">  </div>
       </section>
       ${Button({ type: 'button', title: 'Logout', class: 'primary-button', onClick: logout })}
     </section>
   `;
 
-    return template;
+  return template;
 }
 
+window.onload = showPosts;
 
 
 
