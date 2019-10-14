0c74d1a224d4d91b838c22edf34e7d03ebd9a27b
GeniferGonzalezSantos <genh_@hotmail.com>
2019-10-14 14:40:00 -0300

    redirecionamento do arquivo cadastro para a home

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,19 +3,16 @@ import Button from '../components/button.js';
 import Input from '../components/input.js';
 
 
-
-
 /* function pegarInput() {
   const email = document.querySelector('.js-email').value;
   const senha = document.querySelector('.js-senha').value;
   console.log(email, senha);
-  
-} */
 
+} */
 /* function validarEmailESenha (){
   if (localStorage.setItem )
 } */
-   // const maximoDeTentativas = 3;
+// const maximoDeTentativas = 3;
 
 function pegarInput() {
   const logado = {
@@ -38,9 +35,9 @@ function pegarInput() {
 // function validarEmailESenha (){
 //   if (localStorage.setItem )
 // }
-  // const maximoDeTentativas = 3;
+// const maximoDeTentativas = 3;
 
-  // const tentativaAtual = 1;
+// const tentativaAtual = 1;
 
 //   while (tentativaAtual <= maximoDeTentativas) {
 
@@ -57,43 +54,48 @@ function pegarInput() {
 //     }
 
 //   }
-// 
+//
 
 
-function Login() {
+function logar() {
   const template = `
-  <nav><img src="fotos/Logo-Base_Caixa_Baixa_V4.png" alt=""></nav>	 
-  <div class="container-login">	   
-    <h1>Base Sustentabilidade</h1>	  
-    <h3>Bem vindo<h3>	
-    <div class="form">	
-    <form>	
-    ${Input({ class: 'js-email', placeholder: 'Email', type: 'email' })}	    
-    ${Input({ class: 'js-senha', placeholder: 'Senha', type: 'password' })}	    
-    ${Button({ title: 'Login',  onClick: pegarInput })}	   
-    </form>	  
-    <p>Você já possui conta? Não, então <a href= "#cadastro">cadastre-se</a>aqui!</p>	 
+  <nav><img src="fotos/Logo-Base_Caixa_Baixa_V4.png" alt=""></nav>
+  <div class="container-login">
+    <h1>Base Sustentabilidade</h1>
+    <h3>Bem vindo<h3>
+    <div class="form">
+    <form>
+    ${Input({ class: 'js-email', placeholder: 'Email', type: 'email' })}
+    ${Input({ class: 'js-senha', placeholder: 'Senha', type: 'password' })}
+    ${Button({ title: 'Login', onClick: pegarInput })}
+    </form>
+    <p>Você já possui conta? Não, então <a href= "#cadastro">cadastre-se</a> aqui!</p>
     </div> 
   `;
   return template;
 }
 
-export default Login;
+export default logar;
 
+function locationHashChanged() {
+  if (location.hash === '#home') {
+    document.querySelector('main').innerHTML = logar();
+  }
+}
+window.addEventListener('hashchange', locationHashChanged, false);
 
 window.validaLogin = (arrayUsuarios, logado) => {
   for (let i = 0; i < arrayUsuarios.length; i++) {
     if (logado.email === logado[i].email
-        && logado.senha === logado[i].senha) {
+      && logado.senha === logado[i].senha) {
       return true;
     }
   }
 };
 
-// const aray = [{email: "w@w", senha: "111"}, {email: "e@e", senha: "222"}, {email: "a@a", senha: "333"}]
-
+// const aray = [{email: "w@w", senha: "111"}
+// {email: "e@e", senha: "222"}, {email: "a@a", senha: "333"}]
 // const user = {email: "w@w", senha: "111"}
-
 // for (let i of aray) {
 //   console.log(i.email === user.email && )
 //   console.log()
