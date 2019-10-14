54ebd0bf03f50db50443a295b65c6d98e8fc8fbe
tiemimaeda <priscila.maeda@yahoo.com.br>
2019-10-14 16:05:37 -0300

    Login with  email and passaword

--- src/main.js
+++ src/main.js
@@ -1,20 +1,23 @@
-// import Home from './pages/home.js';
+import Login from './pages/home.js';
 import Register from './pages/register.js';
 import NewPage from './pages/newpage.js';
 
 function pageRoute() {
-  const pageName = (window.location.hash) ? window.location.hash : '#register';
+  const pageName = (window.location.hash) ? window.location.hash : '#home';
   switch (pageName) {
-    case '#register':
-      document.querySelector('main').innerHTML = Register();
+    case '#home':
+      document.querySelector('main').innerHTML = Login();
       break;
     case '#newpage':
       document.querySelector('main').innerHTML = NewPage();
       break;
-    default:
+    case '#register':
       document.querySelector('main').innerHTML = Register();
+      break;
+    default:
+      document.querySelector('main').innerHTML = Login();
   }
 }
 
 window.addEventListener('load', pageRoute);
-window.addEventListener('hashchange', pageRoute, false);
+window.addEventListener('hashchange', pageRoute, false);  
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,14 +1,62 @@
 import Button from '../components/button.js';
+import Input from '../components/input.js';
 
-function Home() {
+// function Home() {
+//   const template = `
+//     <h1>Home Page</h1>
+//     ${Button({ id: '🐠', title: 'Botão 🐠' })}
+//     ${Button({ id: '🎉', title: 'Botão 🎉' })}
+//     <p>Esse é um exemplo 🍌</p>
+//   `;
+
+//   return template;
+// }
+
+// export default Home;
+
+function signInWithEmailAndPassword() {
+  const email = document.querySelector('.js-email-input').value;
+  const password = document.querySelector('.js-password-input').value;
+
+  firebase.auth().signInWithEmailAndPassword(email, password)
+
+    .then((logedin) => {
+      alert(`Bem vindo ${email}`);
+      window.location.hash = '#newpage';
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      alert('Usuario nao cadastrado');
+      window.location.hash = '#register';
+
+    // ...
+    });email - password.html
+}
+
+function Login() {
   const template = `
-    <h1>Home Page</h1>
-    ${Button({ id: '🐠', title: 'Botão 🐠' })}
-    ${Button({ id: '🎉', title: 'Botão 🎉' })}
-    <p>Esse é um exemplo 🍌</p>
-  `;
-
+  <h1>entrar </h1>
+  ${Input({
+    class: 'js-email-input',
+    placeholder: 'Email',
+    type: 'email',
+  })}
+  ${Input({
+    class: 'js-password-input',
+    placeholder: 'Senha',
+    type: 'password',
+  })}
+<br>
+<br>
+  ${Button({
+    id: 'LoginAccount',
+    title: 'Entrar',
+    onClick: signInWithEmailAndPassword,
+  })}
+`;
   return template;
 }
 
-export default Home;
+export default Login;
+
