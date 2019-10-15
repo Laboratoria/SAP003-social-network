775fb52752628ec5f1102f85ed05614404b28465
milaferreira <camila_adventista@hotmail.com>
2019-10-15 12:31:16 -0300

    logica login e cadastro atualizados

--- src/pages/cadastro.js
+++ src/pages/cadastro.js
@@ -1,45 +1,44 @@
 import Input from '../components/input.js';
 import Button from '../components/button.js';
 
+
 function pegarInput() {
+  let dadoslocal = JSON.parse(localStorage.getItem('cadastro'));
+
   const dados = {
     email: document.querySelector('.js-email').value,
     nome: document.querySelector('.js-nome').value,
     senha: document.querySelector('.js-senha').value,
   };
 
-  /*  window.localStorage.setItem('usuario', JSON.stringify(dados));
-   arrayUsuarios.push(dados); */
-  let usuarios = localStorage.getItem('arrayUsuarios');
-  usuarios = JSON.parse(usuarios);
-  if (!Array.isArray(usuarios)) {
-    usuarios = [];
+  if (!dadoslocal) {
+    dadoslocal = [dados];
+  } else {
+    dadoslocal.push(dados);
   }
-  usuarios.push(dados);
-  localStorage.setItem('arrayUsuarios', JSON.stringify(usuarios));
-  localStorage.setItem('usuarioLogado', JSON.stringify(dados));
-  window.location.hash = '#home';
+
+  window.localStorage.setItem('cadastro', JSON.stringify(dadoslocal));
 }
 
-function cadastrar() {
+function Cadastro() {
   const template = `
-  <div class="cadastro">
-  <form class="container-cadastro">
+  <h1><a href='#login'>Login</a></h1>
     ${Input({ class: 'js-email', placeholder: 'Email', type: 'email' })}
     ${Input({ class: 'js-nome', placeholder: 'Nome completo', type: 'text' })}
     ${Input({ class: 'js-senha', placeholder: 'senha', type: 'password' })}
     ${Button({ title: 'Cadastre-se', onClick: pegarInput })}
- </form>
- </div>
-`;
+  `;
   return template;
 }
 
-export default cadastrar;
-
 function locationHashChanged() {
   if (location.hash === '#cadastro') {
-    document.querySelector('main').innerHTML = cadastrar();
+    document.querySelector('main').innerHTML = Cadastro();
+  } else if (location.hash === '#login') {
+    document.querySelector('main').innerHTML = Login();
   }
 }
+
 window.addEventListener('hashchange', locationHashChanged, false);
+
+export default Cadastro;
