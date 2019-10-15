e20d184f7174a6a006e8fe7a3ec802d6ecd02aa7
carolrosatto <carolrosatto@gmail.com>
2019-10-15 17:17:08 -0300

    post feed

--- src/pages/feed.js
+++ src/pages/feed.js
@@ -1,24 +1,20 @@
 import Button from '../components/button.js';
+import Textarea from '../components/textarea.js';
 
 function Feed(posts) {
-  posts.forEach()
-  const loading = document.querySelector('.loading');
-  loading.innerHTML= "Carregando..."
 
-  // collectionPost.get().then(snap => {
-  //   console.log("SEGUNDO")
-  //   loading.innerHTML= ""
-  // // listPost.innerHTML=""
-  //   // snap.forEach(post => {
-  //   // addingPost(post)
-  //   // })
-  // });
+  const loading = document.querySelector('.loading');
+  const collectionPost = firebase.firestore().collection('posts');
+  loading.innerHTML= "Carregando..."
+  collectionPost.get().then(snap => {
+    loading.innerHTML= ""
+    snap.forEach(post => addingPost(post));
+  });
 
   const template =`
     <form id ="formPost">
-    <textarea class='Text1' placeholder=''></textarea>
-    <p class="login"></p>
-    ${Button({ class: 'mytext', onclick:formPost, title: 'ENVIAR' })}
+    ${Textarea({ class: "post-input", placeholder: ' '})}
+    ${Button({ class: 'mytext', onclick:formPost,  title: 'ENVIAR' })}
     </form>
     <ul id="posts"></ul>
     `
@@ -27,43 +23,28 @@ function Feed(posts) {
   
 
 function formPost(){
-  const text = document.querySelector('.Text1').value;
+  const text = document.querySelector('.post-input').value;
   const post = {
     likes : 0,
     comments:[],
     text: text,
-    // timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+    timestamp: firebase.firestore.FieldValue.serverTimestamp()
   }
-  firebase.firestore().collection('posts').add(post).then(res => {
-    // text.value
-    // Post(res)
-  })
-}
-  
- /*function Post(){
-   const listPost = document.querySelector('#posts');
-   const collectionPost = firebase.firestore().collection('posts')
-   listPost.innerHTML= "Carregando"
-   collectionPost.get().then(snap => {
-     console.log(snap)
-     listPost.innerHTML=""
-     snap.forEach(post => {
-     addingPost(post)
-     })
-   })
- }*/
+  firebase.firestore().collection('posts').add(post)
+  }
 
+  
 function addingPost(post){
   const listPost = document.querySelector('#posts');
   const templatePost = `
   <li>
-  ${post.data().timestamp.toDate().toLocaleString('pt-BR')}:
+  ${post.data().timestamp.toDate().toLocaleString('pt-BR')}
   ${post.data().text}
   ${post.data().likes}
   </li>
   `
   listPost.innerHTML += templatePost
-
+  
 }
 
 export default Feed;
