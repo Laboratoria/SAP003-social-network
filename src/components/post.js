49988989a1b294aea4e2abe80bbabce4d4fec444
claudiakemi <claudiakemis@gmail.com>
2019-10-15 17:54:18 -0300

    feed armazenando, mas não printando

---
+++ src/components/post.js
@@ -0,0 +1,10 @@
+function Post(props) {
+    const template = `<textarea rows='8'
+    cols='50' placeholder='${props.placeholder}'
+    class='${props.class}'> </textarea>`;
+
+    return template;
+  };
+
+
+  export default Post;
