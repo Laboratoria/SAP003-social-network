862152d0e3b93aac1e98ab4f17e3ef780a36f791
MiSilvaSouza <mirian.s2@hotmail.com>
2019-10-15 18:17:13 -0300

    correção no post

--- src/pages/post.js
+++ src/pages/post.js
@@ -1,31 +1,41 @@
+window.app = {
+  loadPost: loadPost,
+};
+
 function savePost() {
 
   const post = document.querySelector('.post').value;
 
   db.collection('post').add({
-    post: post,    
+    post: post,
+    //user_id: id.data,    
   })
-  .then(function(docRef) {
-    //console.log('Document written with ID: ', docRef.id);
-    db.collection('post').get()
-    .then((snap) => {snap.forEach(doc => {
-      
-      const feed = `
-      <li>
-      ${doc.data().post}
-      </li>
-      `
-      document.querySelector('.feed').innerHTML += feed;
-           
-      
-      console.log(`${doc.data().post}`);        
-    });
-    });   
+  .then(function (docRef) {
+    app.loadPost()
   })
-  .catch(function(error) {
-    console.error('Error adding document: ', error);
-  });
-  
-}
+  document.querySelector('.post').value = '';  
+};
+
+function addPost(post) {
+  const feed = document.querySelector('.feed');
+  const feedPost = `
+  <li>
+  ${post.data().post}
+  </li>
+  <br>
+  `
+  feed.innerHTML += feedPost;
+};
+
+function loadPost() {
+  db.collection('post').get()
+  .then((snap) => {
+    document.querySelector('.feed').innerHTML = '';
+    snap.forEach(post => {
+      addPost(post)
+    })
+  })
+};
+
 
 export default savePost;
