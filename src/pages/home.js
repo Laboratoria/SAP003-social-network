6d8a2305a82fdc0f32b75461badb2fa15c94a29b
Joanilza Bockhorny <jbockhorny@gmail.com>
2019-10-16 14:16:53 -0300

    mexendo

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,9 +1,14 @@
 import Button from '../components/button.js';
 import Textarea from '../components/textarea.js';
 
+var userCollection = db.collection('users').doc('user.uid');
+console.log(userCollection);
+
 function feed() {
   const template = `
     <img src="../../imagens/logo.png"></img class="image-logo">
+    <p>Bem vinda!</p>
+    <p>O que você deseja postar agora...</p>
     ${Textarea({ id: 'post', class: 'post' })}
     ${Button({ id: 'publish', title: 'Publish'})}
     `;
