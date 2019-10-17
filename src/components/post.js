d1592051d1a68c86ea4e33603ebcde5e16724665
anacola <anac.cola@gmail.com>
2019-10-17 15:48:01 -0300

    delete button ok

--- src/components/post.js
+++ src/components/post.js
@@ -1,9 +1,26 @@
+import Button from './button.js';
+
 function Post(props) {
-  return `<div class="post" id="${props.id}">
+  return `<div class="post" data-id="${props.dataId}">
   <span class="post-username">${props.username}</span>
   <span class="post-date">${props.date}</span>
   <span class="post-text">${props.text}</span>
+  ${Button({
+    dataId: props.dataId,
+    class: 'primary-button',
+    onClick: window.post.deletePost,
+    title: 'Deletar',
+  })}
   </div>`;
+}
+
+function deletePost (event){
+  const id = event.target.dataset.id;
+  firebase.firestore().collection('post').doc(id).delete();
+  event.target.parentElement.remove();
+}
+window.post ={
+  deletePost,
 }
 
 export default Post;
