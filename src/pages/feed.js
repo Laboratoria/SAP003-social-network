a1cdaeab29523134fe9cf2c3a2ef5a385855a376
mokasousa <moka.sousa@gmail.com>
2019-10-17 17:09:00 -0300

    onsnapshot funcinando

--- src/pages/feed.js
+++ src/pages/feed.js
@@ -1,6 +1,64 @@
 import Button from '../components/button.js';
 import Input from '../components/input.js';
 
+function createPost() {
+  const text = document.querySelector('.text-area').value;
+  const post = {
+    likes: 0,
+    text,
+    comments: [],
+    user_id: firebase.auth().currentUser.displayName,
+    createdAt: firebase.firestore.FieldValue.serverTimestamp()
+  };
+  const postsCollection = firebase.firestore().collection('posts');
+  postsCollection.add(post)
+    .then(() => {
+      console.log('Post criado com sucesso!');
+      //text = ''
+    })
+    .catch((error) => {
+      console.log('erro', error);
+      console.log('Não foi possível criar post.');
+    });
+}
+
+function addPost(post, postId) {
+  const postTemplate = `
+      <li id="${postId}">
+
+        <div>${post.createdAt.toDate().toLocaleString('pt-BR')}</div>
+        ${post.text} 
+        <div class="interaction-area">
+          Likes:${post.likes}
+          <div class="coments">
+            <span class='fa fa-comments'></span>
+          </div>
+        </div>
+      </li>
+  `;
+  return postTemplate;
+}
+
+function logOut() {
+  auth
+    .signOut()
+    .then(() => {
+      console.log('adeus');
+      window.location = '#login';
+    });
+}
+
+function userInfo() {
+  //console.log("oi", firebase.auth().currentUser);
+  const user = auth.currentUser;
+  db.collection('users').doc(user.uid).get().then(doc => {
+    const username = `
+    <h4>${doc.data().name}</h4>
+    <p>${user.email}</p>
+    `;
+    document.querySelector('.profile').innerHTML = username;
+  });  
+}
 
 function NewPostTemplate() {
   const postArea = `
@@ -30,75 +88,22 @@ function NewPostTemplate() {
 
 function loadPosts() {
   const postsCollection = firebase.firestore().collection('posts');
-  postsCollection.get()
-    .then((res) => {
-      const postList = document.querySelector('.post-list');
-      postList.innerHTML = '';
-      res.forEach((post) => {
-        postList.innerHTML += addPost(post.data());
-      });
-    });
-}
+  console.log(typeof firebase.firestore.FieldValue.serverTimestamp());
+  postsCollection.orderBy('createdAt', 'desc').onSnapshot((snapshot) =>{
 
-function createPost() {
-  const text = document.querySelector('.text-area').value;
-  const post = {
-    likes: 0,
-    text,
-    comments: [],
-    user_id: firebase.auth().currentUser.uid,
-  };
-  const postsCollection = firebase.firestore().collection('posts');
-  postsCollection.add(post)
-    .then(() => {
-      console.log('Post criado com sucesso!');
-      loadPosts();
-      postsCollection.get();
+    const postList = document.querySelector('.post-list');
+    postList.innerHTML = '';
+    //console.log(snapshot.docs)
+    snapshot.docs.forEach((post) => {
+      //console.log(post.data().id)
+      postList.innerHTML += addPost(post.data(), post.id);
     })
-    .catch((error) => {
-      console.log('erro', error);
-      console.log('Não foi possível criar post.');
-    });
-}
-
-function addPost(post) {
-  const postTemplate = `
-  <section class="print-post">
-    <ul class="post-list">
-      <li id="${post.id}">
-        ${post.text} 
-        <div class="interaction-area">
-          Likes:${post.likes}
-          <div class="coments">
-            Comentarios
-          </div>
-        </div>
-      </li>
-    </ul>    
-  </section
-  `;
-  return postTemplate;
-}
-
-function logOut() {
-  auth
-    .signOut()
-    .then(() => {
-      console.log('adeus');
-      window.location = '#login';
-    });
-}
-
-function userInfo() {
-  console.log("oi", firebase.auth().currentUser);
-  const user = auth.currentUser;
-  db.collection('users').doc(user.uid).get().then(doc => {
-    const username = `
-    <h4>${doc.data().name}</h4>
-    <p>${user.email}</p>
-    `;
-    document.querySelector('.profile').innerHTML = username;
-  });  
+    // snapshot.docChanges().forEach((change) =>{
+    //   let updates = change.doc.data();
+    //   //console.log(updates);
+    //   postList.innerHTML += addPost(updates);
+    // })
+  });
 }
 
 function Feed() {
@@ -110,15 +115,13 @@ function Feed() {
       onclick:logOut, 
       title: 'Sair'
     })}
-    <div class='profile'></div>
+    <div class='profile'>${userInfo()}</div>
       ${NewPostTemplate()}
-      <ul class='post-list'></ul>
+      <section class="print-post">
+      <ul class='post-list'>${loadPosts()}</ul>
+      </section>
   `;
-  userInfo();
-  loadPosts();
   return template;
 }
 
 export default Feed;
-
-window.loadPosts = loadPosts;
