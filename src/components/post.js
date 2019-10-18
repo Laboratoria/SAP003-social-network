9b9eb36c7abd9b1d9c5680711d4addc020272029
RachelMarotta <kelmarotta@icloud.com>
2019-10-18 12:59:55 -0300

    css botões editar e deletar

--- src/components/post.js
+++ src/components/post.js
@@ -4,6 +4,8 @@ import Button from './button.js';
 const Post = (props) => {
   const template = `
   <div class="container-public">
+  <p data-id='${props.id}' contentEditable="true" class='publication'>${props.post.text}
+    </p>
     <div class="info-post">
     <p data-id='${props.id}' class='date-post'>${props.post.addedAt.slice(0, 16)}</p> 
        ${Button({
@@ -19,8 +21,6 @@ const Post = (props) => {
     onClick: props.deleteEvent,
   })} 
   </div>
-  <p data-id='${props.id}' contentEditable="true" class='publication'>${props.post.text}
-    </p>
 </div>`;
 
   return template;
