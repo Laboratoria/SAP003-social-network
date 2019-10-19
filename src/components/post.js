2fee8515e2f886dfb13e9605f7f6512e61af27f5
Mariana Ribeiro <mariana-fribeiro@live.com>
2019-10-19 20:35:02 -0300

    finished basic home css

--- src/components/post.js
+++ src/components/post.js
@@ -3,60 +3,59 @@ import Textarea from './textarea.js';
 
 function Post(props) {
   return `<div class="post" data-id="${props.dataId}">
-  <span class="post-username">${props.username}</span>
-  <span class="post-date">${props.date}</span>
-  <span class="post-text" id="${props.dataId}" >${props.text}</span>
+  <span class="post-username primary-font">${props.username}</span>
+  <span class="post-date secondary-font">${props.date}</span>
+  <span class="post-text secondary-font" id="${props.dataId}" >${props.text}</span>
   ${Button({
     dataId: props.dataId,
-    class: 'secundary-button',
+    class: 'secondary-button primary-font',
     onClick: window.post.deletePost,
     title: 'Deletar',
   })}
 
   ${Button({
     dataId: props.dataId,
-    class: 'secundary-button',
+    class: 'secondary-button primary-font',
     onClick: window.post.editPost,
     title: 'Editar',
   })}
 
   ${Button({
     dataId: props.dataId,
-    class: 'secundary-button',
+    class: 'secondary-button primary-font',
     onClick: window.post.saveEditPost,
     title: 'Salvar',
   })}
   </div>`;
 }
 
-function deletePost (event){
+function deletePost(event) {
   const id = event.target.dataset.id;
   firebase.firestore().collection('post').doc(id).delete();
   event.target.parentElement.remove();
 }
 
-const editPost = () => {
+function editPost(event) {
   const id = event.target.dataset.id;
-  const postText = document.getElementById(id)
+  const postText = document.getElementById(id);
   postText.setAttribute('contentEditable', 'true');
- 
+  postText.style.backgroundColor = 'red';
+}
 
-};
-
-const saveEditPost= ()=> {
+function saveEditPost(event) {
   const id = event.target.dataset.id;
   const saveText = document.getElementById(id)
-  const newText = saveText.textContent
+  const newText = saveText.textContent;
   firebase.firestore().collection('post').doc(id).update({
     text: newText,
   });
   saveText.setAttribute('contentEditable', 'false');
-
 }
-window.post ={
+
+window.post = {
   deletePost,
   editPost,
-  saveEditPost
-}
+  saveEditPost,
+};
 
 export default Post;
