88387f56934f5164f364115476e385b2631b67ec
vanesnery <nery_van@hotmail.com>
2019-10-19 02:49:06 -0300

    ajustando layout desktop e a funçao mostrar em tela

--- src/pages/feed.js
+++ src/pages/feed.js
@@ -1,92 +1,101 @@
 import Button from '../components/button.js';
 import Post from '../components/post.js';
 
-const dataBase = firebase.firestore();
-
 function signOut() {
-  firebase.auth().signOut().then(function () {
+  firebase.auth().signOut().then(() => {
     window.location.hash = '#login';
-    alert('Agora tu saiu.')
-  })
-};
+    alert('Agora tu saiu.');
+  });
+}
 
 function profile() {
   window.location.hash = '#perfil';
-};
+}
 
 function Posts() {
   const dataBase = firebase.firestore();
-  //const id = firebase.auth().currentUser.uid;  
+  const id = firebase.auth().currentUser.uid;
   const textInput = document.querySelector('.textarea');
   const post = {
     timestamp: new Date().toLocaleDateString('pt-BR') + ':' + new Date().getHours() + ':' + new Date().getMinutes() + ':' + new Date().getSeconds(),
     text: textInput.value,
     likes: 0,
     coments: [],
-    user_id: firebase.auth().currentUser.uid
-  }
-  console.log(post);
+    user_id: id,
+  };
   dataBase.collection('posts').add(post)
     .then((docRef) => {
-      document.querySelector('#timeline').insertAdjacentHTML('afterbegin', `
-    <li data-id='${docRef.id}'>
-      ${window.button.component({
-        dataId: docRef.id,
-        title: 'Deletar',
-        onClick: window.feed.deletePost,
-      })} 
-      
-    </li>`
-      );
-    }
-    )
-
-};
+      document.querySelector('.timeline').insertAdjacentHTML('afterbegin', `
+    <ul data-id='${docRef.id}'>
+    ${text}
+    ${window.button.component({
+      dataId: docRef.id,
+      class: 'primary-button',
+      title: 'X',
+      onClick: window.feed.deletePost,
+    })} 
+    </ul> `);
+    });
+}
 
 function deletePost(event) {
   const id = event.target.dataset.id;
   firebase.firestore().collection('posts').doc(id).delete();
   event.target.parentElement.remove();
-  // document.querySelector(`li[data-id='${id}']`).remove();
 }
 
-
-function Feed() {
-  let postTemplate = '';
-  //const dataBase = firebase.firestore();
- //document.getElementById('timeline').innerHTML = 'Carregando...';
- firebase.firestore().collection('posts').orderBy('timestamp', 'desc').get().then((snap) => {
-  //document.getElementById('timeline').innerHTML = '';
-     
-    snap.forEach((post) => {
-      postTemplate += `
-      <div data-id='${post.id}' class='postMessage' id='${post.id}'>
-      ${post.data().timestamp}:
+function feed() {
+  let posTemplate = '';
+  firebase.firestore().collection('posts').orderBy('timestamp', 'desc').get()
+    .then((snap) => {
+      snap.forEach((post) => {
+        posTemplate += `<section data-id='${post.id}' class='postMessage'>
+      ${post.data().timestamp}- 
       ${post.data().user_id}
       ${post.data().text}
       ${post.data().likes}
-      ${Button({ dataId: post.id, title: 'Deletar', onClick: deletePost })}
-      </div>
-      `;
-      //document.getElementById('timeline').innerHTML += postTemplate;
-
+      ${Button({
+    dataId: post.id,
+    title: 'Deletar',
+    onClick: deletePost,
+  })}
+      </section>`;
+      });
     });
 
   const template = `
   <h1>Feed</h1>
-  ${Button({ title: 'Sair', class: 'primary-button', onClick: signOut })}
-  ${Button({ title: 'Perfil', class: 'primary-button', onClick: profile })}
-   <h2>Post</h2>
-  ${Post({ class: 'textarea', id: 'post-textarea', placeholder: 'Escreva aqui', type: 'text' })}
-  ${Button({ title: 'Postar', class: 'primary-button', onClick: Posts })}
-  <section id='timeline'>${postTemplate}</section>
+  ${Button({
+    title: 'Sair',
+    class: 'primary-button',
+    onClick: signOut,
+  })}
+  ${Button({
+    title: 'Perfil',
+    class: 'primary-button',
+    onClick: profile,
+  })}
+  <h2>Post</h2>
+  <div class='post'>
+  ${Post({
+    class: 'textarea',
+    id: 'post-textarea',
+    placeholder: 'Escreva aqui',
+    type: 'text',
+  })}
+  ${Button({
+    title: 'Postar',
+    class: 'primary-button',
+    onClick: Posts,
+  })}
+  <div>
+  <li class='timeline'>${posTemplate}</li>
   `;
   return template;
-})}
+}
 
 window.feed = {
-    deletePost
+  deletePost,
 };
 
-
-export default Feed;
+export default feed;
--- src/pages/login.js
+++ src/pages/login.js
@@ -5,24 +5,25 @@ function AuthEmailPassButton() {
   const email = document.querySelector('.js-email-input').value;
   const password = document.querySelector('.js-password-input').value;
   firebase.auth().signInWithEmailAndPassword(email, password)
-    .then(function () {
-      window.location.hash = '#feed';
-      alert('Autenticado ' + email);
-  })
-  .catch(function (error) {
-      console.error(error.code);
-      console.error(error.message);
-      alert('Falha ao autenticar, verifique o e-mail e senha inseridos.')
-  });
+    .then((user) => {
+      if (user) {
+        window.location.hash = '#home';
+        }
+      })
+        .catch((error) => {
+          console.error(error.code);
+          console.error(error.message);
+          alert('Falha ao autenticar, verifique o e-mail e senha inseridos.');
+    })
 }
 
-function Google (){
+function Google() {
   var provider = new firebase.auth.GoogleAuthProvider();
-  firebase.auth().signInWithPopup(provider).then(function(result) {
+  firebase.auth().signInWithPopup(provider).then((result) => {
     var token = result.credential.accessToken;
     var user = result.user;
     window.location.hash = '#feed';
-  }).catch(function(error) {
+  }).catch((error) => {
     var errorCode = error.code;
     var errorMessage = error.message;
     var email = error.email;
@@ -33,13 +34,29 @@ function Google (){
 function Login() {
   const template = `
     <img class='logo' src='logo1.png'/>
-    <h2 class='content'>Seja bem-vinda(o)!</h2>
+    <h2 id='h2' class='content'>Seja bem-vinda(o)!</h2>
     <form class='content'>
-    ${Input({ class:'js-email-input', placeholder:'E-mail', type:'email', })}
-    ${Input({ class:'js-password-input', placeholder:'Senha', type:'password', })}
-    ${Button({ class:'primary-button', title: 'Log In', onClick: AuthEmailPassButton})}
+    ${Input({
+    class: 'js-email-input',
+    placeholder: 'E-mail',
+    type: 'email',
+  })}
+    ${Input({
+    class: 'js-password-input',
+    placeholder: 'Senha',
+    type: 'password',
+  })}
+    ${Button({
+    class: 'primary-button',
+    title: 'Log In',
+    onClick: AuthEmailPassButton,
+  })}
     <p>Ou entre com sua conta...</p>
-    ${Button({ class: 'google-btn', title: '', onClick: Google})}
+    ${Button({
+    class: 'google-btn',
+    title: '',
+    onClick: Google,
+  })}
     <p>Ainda não tem uma conta?</p>
     <a href='#register'>Registre-se!</a>
     </form>`;
