db133003ac559c36f40f6e486de6e2b6e94b2cea
AlineMussi <souzamussi@gmail.com>
2019-10-22 17:02:41 -0300

    'atualizacao feed'

--- src/main.js
+++ src/main.js
@@ -1,24 +1,50 @@
 import Login from './pages/login.js';
 import Register from './pages/register.js';
-import Button from '../components/button.js';
+//import Button from '../components/button.js';
 import Feed from './pages/feed.js';
 
+
+
 function init() {
-  document.querySelector('main').innerHTML = Login();
+  firebase.auth().onAuthStateChanged((user) =>{
+    if (user){
+      if (window.location.hash === '#feed'){
+        firebase.firestore().collection('post')
+        
+        .where('user', '==', user.id)
+        .get()
+        .then((querySnapshot) => {
+          document.querySelector('main').innerHTML=Feed({
+            post:querySnapshot,
+          });
+        });
+      } else if (window.location.hash ==='#login'){
+        document.querySelector('main').innerHTML = Login();
+      } else {
+        window.location.hash ==='#register';
+        document.querySelector('main').innerHTML = Register();
+      }
+    } else {
+      window.location.hash ==='#register';
+      document.querySelector('main').innerHTML = Register();
+    }
+  });
+
 }
 
 window.addEventListener('load', init);
+window.addEventListener('hashchange', init);
 
-const pages = {
+/*const pages = {
   login: Login(),
   register: Register(),  
   feed: Feed(),
 };
 window.addEventListener('hashchange', () => {
   document.querySelector('main').innerHTML = pages[location.hash.substring(1)];
-},false);
+},false);*/
 
-// Your web app's Firebase configuration
+//Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyANqYMYjH5L6AIG9_Vqkq_cwwHj3nhpZjg",
   authDomain: "rede-social-fake.firebaseapp.com",
@@ -29,7 +55,6 @@ const firebaseConfig = {
   appId: "1:752269784715:web:ce0003abf7c7e754cd068e",
   measurementId: "G-72B9LZ4VES"
 };
-// Initialize Firebase
+//Initialize Firebase;
 firebase.initializeApp(firebaseConfig);
-//firebase.analytics();
-
+firebase.analytics();
--- src/pages/feed.js
+++ src/pages/feed.js
@@ -1,60 +1,122 @@
-/* eslint-disable indent */
 import Button from '../components/button.js';
 import Input from '../components/input.js';
 import Div from '../components/div.js';
-//import Div from '../components/div.js';
-//import Card from '../components/card.js';
 
 function Feed() {
   const template = `
     <h1>Rede Social</h1>
     ${Input({
     class: 'post-input',
-    type: 'text'
+    type: 'text',
+    placeholder: 'comente algo'
   }) +
     Button({
       title: 'enviar',
-      onClick: loadData
+      onClick: saveData
     }) +
-    Div({
-      class: 'insert-post'
+    Div ({
+      class:'insert-post',
     })}`;
 
   return template;
 }
 
+export default Feed;
 
-function loadData() {
-  const postlist = document.querySelector('.post-input').value;
-  //cria coleção de post no firebase/ ''post' representa o objeto que será criado no banco de dados firebase storage
-  firebase.firestore().collection('post').add({
-    text: postlist,
-    userID: firebase.auth().currentUser.uid,
-    //método JS para pegar a data 
-    addeAt: (new Date()).toISOString(),
-  }).then(result => alert('oi'))
-  document.querySelector  ('insert-post').innerHTML = text;
-}
-
-function addPost(post) {
-  const postTemplate = `<li>
-    ${post.data().text}
-    </li>
-    `
-    document.querySelector('.insert-post').innerHTML += postTemplate;
-}
-
-const loadPost = () => {
+/*function loadPost () {
+  const postList = document.querySelector('.post-input');
   const postCollection = firebase.firestore().collection('post');
+  // postList.innerHTML = 'Carregando...'
   postCollection.get().then(snap => {
+    postList.innerHTML = ""
     snap.forEach(post => {
       addPost(post)
     })
   })
+}*/
+
+function saveData() {
+  
+  const postlist = document.querySelector('.post-input').value;
+  
+  const post = {  
+    text: postlist,
+    userID: firebase.auth().currentUser.uid,
+    likes: 0,
+    addeAt: new Date().toISOString()
+  }
+  firebase.firestore().collection('post').add(post).then(result => document.querySelector('.insert-post').innerHTML +=`<section>
+<p>${post.text}</p>
+</section>` 
+)
 }
 
 
-//loadPost()
+/*function addPost(post) {
+    const postTemplate = `<li>
+    ${post.data().text}
+    ${post.data().likes}
+    </li>
+    `;
+}*/
 
 
-export default Feed;
+// import Button from '../components/button.js';
+// import Input from '../components/input.js';
+
+// function sendPost() {
+//   const text = document.querySelector('.js-text').value;
+//   const id = firebase.auth().currentUser.uid;
+//   firebase.firestore().collection('post').add({ text, user: id })
+//     .then((docRef) => {
+//       document.querySelector('ul').insertAdjacentHTML('afterbegin', `
+//         <li data-id='${docRef.id}'>
+//         ${text}
+//         ${window.button.component({
+//             dataId: docRef.id,
+//             title: 'Deletar',
+//             onClick: window.home.deletePost,
+//           })}
+//         </li>
+//       `);
+//       console.log('Document written with ID: ', docRef.id);
+//     });
+// }
+
+// function deletePost(event) {
+//   const id = event.target.dataset.id;
+//   firebase.firestore().collection('post').doc(id).delete();
+//   event.target.parentElement.remove();
+//   document.querySelector(`li[data-id='${id}']`).remove();
+// }
+
+// function Feed(props) {
+//   let postsLayout = '';
+//   props.posts.forEach((doc) => {
+//     postsLayout += `
+//       <li data-id='${doc.id}'>
+//         ${doc.data().text}
+//         ${Button({ dataId: doc.id, title: 'Deletar', onClick: deletePost })}
+//       </li>
+//     `;
+//   });
+
+// const template = `
+//     <h1>Home Page</h1>
+//     <form>
+//      ${Input({ class: 'js-text', type: 'text', placeholder: 'Digite aqui'})}
+//       ${Button({ id: '🐠', title: 'Botão 🐠', onClick: sendPost })}
+//     </form>
+//     <ul>${postsLayout}</ul>
+// 	  `;
+
+
+//   return template;
+// }
+
+
+// window.feed = {
+//   deletePost,
+// };
+
+//export default Feed;
