2e3df90c374339d013a59511f0f48377bb69b75c
GeniferGonzalezSantos <genh_@hotmail.com>
2019-10-23 11:14:49 -0300

    modificar layout no login e tentativa de carrossel no cadastro (fracasso semmm orr)

--- src/pages/home.js
+++ src/pages/home.js
@@ -31,24 +31,23 @@ function pegarInput() {
 
 function logar() {
   const template = `
-<img src="fotos/Logo-Base_Caixa_Baixa_V4.png" alt="">
-<div class="container-login">
+ 
+  <div class="container-login">
+  <img src="fotos/base.png" alt="" >
+  <div class="titulo">
   <h1 id="title">Base Sustentabilidade</h1>
-  <h3>Bem-vindo<h3>
+  <h2>Bem-vindo</h2>
   <p id="erro" class="erro"></p>
-  <div>
   <form class="form">
   ${Input({ class: 'js-email', placeholder: 'Email', type: 'email' })}
   ${Input({ class: 'js-senha', placeholder: 'Senha', type: 'password' })}
   ${Button({ title: 'Login', onClick: pegarInput })}
   </form>
   <p>Você já possui conta? Não, então <a href= '#cadastrar'>cadastre-se</a> aqui!</p>
-  </div> 
+  </div>
+  </div>
 
-  </div>
-  <div class="img-footer">
-  <img src="fotos/logo-half-2.png" alt="">
-  </div>
+
 `;
   return template;
 }
