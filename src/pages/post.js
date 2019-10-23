55e96b4e9bab2f90580f0524c820f23a0c3bfbb3
fernanda <ono.frnd@gmail.com>
2019-10-23 10:41:17 -0300

    desconsiderar branch

--- src/pages/post.js
+++ src/pages/post.js
@@ -11,6 +11,7 @@ function savePost() {
 
   db.collection('post').add({
     post: post,
+    idname: firebase.auth().currentUser.displayName,
     likes: 0,
     comments: [],
     uid: uid,
@@ -26,29 +27,29 @@ function addPost(post) {
   const feed = document.querySelector('.feed');
   const feedPost = `
   <li data-id= '${post.id}' class="post-list">
-  <span class="date-hour">${post.data().timestamp.toDate().toLocaleString('pt-BR')}</span>
-
-  <br>  
-  ${post.data().post}
-  <br>  
-  <br>
-  <p class="border"></p>
-  ${Button({ class: "button-feed", onClick: savePost, title:'💛' })} 
-  ${post.data().likes}
-  ${Button({ class: "button-feed", onClick: loadComments(post), title:'💬' })}
-  <p class="border"></p>
-  ${post.data().comments}
-  <br>
-
-    </li>
-  <br>
-  `
+    <span class= "idname">${post.data().idname}</span>
+    <span class="date-hour">${post.data().timestamp.toDate().toLocaleString('pt-BR')}</span>
+    <p class="border"></p>
+    ${post.data().post}
+    <br>  
+    <br>
+    <p class="border"></p>
+    ${Button({ class: "button-feed", onClick: getLike, title:'💛' })} 
+    ${post.data().likes}
+    ${Button({ class: "button-feed", onClick: loadComments, title:'💬' })}
+    <p class="border"></p>
+    <span class= "idname"> Comentários </span>
+    <br>
+    ${post.data().comments}
+    <br>
+  </li>
+    `
   feed.innerHTML += feedPost;
 };
 
 function loadPosts() {  
   db.collection('post').orderBy('timestamp', 'desc').get()
-  .then((snap) => {
+  .then(snap => {
     document.querySelector('.feed').innerHTML = '';
     snap.forEach(post => {
       addPost(post)
@@ -56,10 +57,20 @@ function loadPosts() {
   })
 };
 
-function loadComments(post){
-  document.querySelector('.feed').innerHTML += `
- 
-  ` 
+function loadComments(){
+   db.collection('post').update({'post':firebase.firestore.FieldValue.arrayUnion("comments")
+  });
+  }
+
+function getLike() {
+    db.collection('post').get().then(snapshot => {
+    let likes = 0;
+      snapshot.forEach(doc => {
+        likes += doc.data().count;
+        });
+
+        return likes;
+    });
 }
 
 export default savePost;
