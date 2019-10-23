5d0938380eddb7c0b77dbb51b4642287bb82c23c
claudiakemi <claudiakemis@gmail.com>
2019-10-23 20:40:21 -0300

    posts editáveis

--- src/pages/feed.js
+++ src/pages/feed.js
@@ -26,28 +26,31 @@ function AddPostToFirebase() {
     user_id: id,
   };
   dataBase.collection('posts').add(post)
-    .then((docRef) => {
-      document.querySelector('.timeline').insertAdjacentHTML('afterbegin', `
-      <li class='postMessage' data-id='${docRef.id}'>
-      ${post.timestamp}
-      ${post.name}
-      ${post.text}
-      ${post.likes}
-      ${post.comments}
-      ${window.button.component({
-        dataId: docRef.id,
-        class: 'primary-button',
-        title: '🗑️',
-        onClick: window.feed.deletePost,
-      })}
-      ${window.button.component({
-        dataId: docRef.id,
-        class: 'primary-button',
-        title: '✏️',
-        onClick: window.feed.editPost,
-      })}
-      </li> `)
-    });
+  .then((docRef) => {
+    document.querySelector('.timeline').insertAdjacentHTML('afterbegin', `
+    <li class='postMessage' data-id='${docRef.id}'>
+    <div class='postHeader'>${post.timestamp}</div>
+    <div class='postHeader'>${post.name} disse:</div>
+    <div id='post_${docRef.id}'>${post.text}</div>
+    ${post.likes}
+    ${post.comments}
+    ${window.button.component({
+      dataId: docRef.id,
+      class: 'primary-button',
+      title: '🗑️',
+      onClick: window.feed.deletePost,
+    })}
+    ${window.button.component({
+      id: 'edit-'+docRef.id,
+      dataId: docRef.id,
+      class: 'primary-button',
+      title: '✏️',
+      onClick: window.feed.editPost
+    })}
+
+    </li> `)
+    console.log(post)
+  });
 }
 
 function deletePost(event) {
@@ -58,33 +61,49 @@ function deletePost(event) {
 
 function editPost(event) {
   const id = event.target.dataset.id;
-  firebase.firestore().collection('posts').doc(id).update();
-  event.target.parentElement.add();
+  document.getElementById('post_'+id).contentEditable = true;
+  document.getElementById('post_'+id).style.border = '1px solid black';
+  document.querySelector('#edit-'+id).innerHTML = '✔️';
+  document.querySelector('#edit-'+id).addEventListener('click', window.feed.saveEdit);
+}
+
+function saveEdit () {
+  const id = event.target.dataset.id;
+  document.getElementById('post_'+id).contentEditable = false;
+  document.getElementById('post_'+id).style.border = '';
+  document.querySelector('#edit-'+id).innerHTML = '✏️';
+  const text = document.querySelector('#post_'+id).textContent;
+  firebase.firestore().collection('posts').doc(id).update({text});
+  document.querySelector('#edit-'+id).removeEventListener('click', window.feed.saveEdit);
 }
 
 function loadFeed () {
   firebase.firestore().collection('posts').orderBy('timestamp', 'desc').get()
-    .then((querySnapshot) => {
-      querySnapshot.forEach((post) => {
+  .then((querySnapshot) => {
+    querySnapshot.forEach((post) => {
       const postsFeed =  `<li data-id='${post.id}' class='postMessage'>
-      ${post.data().timestamp}-
-      ${post.data().user_id} disse:
-      ${post.data().text}
+      <div class='postHeader'>${post.data().timestamp}-</div>
+      <div class='postHeader'>${post.data().user_id} disse:</div>
+      <div id='post_${post.id}'>${post.data().text}</div>
       ${post.data().likes}
       ${Button({
-    dataId: post.id,
-    title: '🗑️',
-    onClick: deletePost,
-  })}
+        dataId: post.id,
+        title: '🗑️',
+        class: 'primary-button',
+        onClick: deletePost,
+      })}
       ${Button({
-      dataId: post.id,
-      title: '✏️',
-      onClick: editPost,
-  })}
-  </li>`;
-  document.querySelector('.timeline').innerHTML += postsFeed;
-      });
+        id: 'edit-'+post.id,
+        dataId: post.id,
+        title: '✏️',
+        class: 'edit-btn primary-button',
+        onClick: editPost,
+
+      })}
+      </li>`;
+      document.querySelector('.timeline').innerHTML += postsFeed;
     });
+  });
 }
 
 function Feed() {
@@ -125,7 +144,8 @@ window.feed = {
   deletePost,
   editPost,
   loadFeed,
-  AddPostToFirebase
+  AddPostToFirebase,
+  saveEdit
 };
 
 export default Feed;
