9edeb46085814081a05a1744c2d7fe9c02948fd8
GeniferGonzalezSantos <genh_@hotmail.com>
2019-10-24 17:40:26 -0300

    retirar rotas do main

--- src/main.js
+++ src/main.js
@@ -13,33 +13,12 @@ function locationHashChanged() {
     case '#cadastrar':
       document.querySelector('main').innerHTML = cadastrar();
       break;
-    case '#item-1':
-        document.querySelector('main').innerHTML = cadastrar();
-        salvar();
-        break;
-    case '#item-2':
-        document.querySelector('main').innerHTML = cadastrar();
-        salvar();
-        break;
-    case '#item-3':
-        document.querySelector('main').innerHTML = cadastrar();
-        salvar();
-        break;
-    case '#item-4':
-          document.querySelector('main').innerHTML = cadastrar();
-          salvar();
-          break;
-    case '#item-5':
-          document.querySelector('main').innerHTML = cadastrar();
-          salvar();
-          break;
     case '#home':
       document.querySelector('main').innerHTML = logar();
       break;
     case '#feed':
       document.querySelector('main').innerHTML = feed();
       salvar();
-     
     default:
       document.querySelector('main').innerHTML = logar();
   }
