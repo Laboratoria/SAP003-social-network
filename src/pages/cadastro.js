321895894dd61bd6797ed2a231e51b2101e9879c
GeniferGonzalezSantos <genh_@hotmail.com>
2019-10-24 17:27:55 -0300

    adicionar arquivo css

--- src/pages/cadastro.js
+++ src/pages/cadastro.js
@@ -28,53 +28,39 @@ function pegarInput() {
 function cadastrar() {
   const template = `
   <div class="container-cadastro">
-  <img src="fotos/mia pose 1-01.png" class="mia"> 
-  <div class="text">
-  <div class="wall wall-1" id="wall-1">
-  <h1>Olá!<br> Eu sou a Mia e vou te contar o porquê precisamos do Base</h1>
-  <a href="#wall-2">Avançar</a>
-  </div>
-  <div class="wall wall-2" id="wall-2">
-  <p>O base, assim como outros aplicativos, utiliza o conceito de crowdsourcing, isso significa que, seus usuários passem as informações criando uma base de dados com avaliações.</p>
-  <a href="#wall-3">Avançar</a>
-  <a href="#wall-1">Voltar</a>
-  </div>
-  <div class="wall wall-3" id="wall-3">
-  <p>Dessa forma, cada um de nós podemos preencher vários questionário de avaliações sobre o nível de sustentabilidade de estabelecimentos comerciais,turísticos e produtos.</p>
-  <a href="#wall-4">Avançar</a>
-  <a href="#wall-2">Voltar</a>
-  </div>
-  <div class="wall wall-4" id="wall-4">
-  <p>Que lugares você tem frequentado?<br>Onde passou as últimas férias?<br>O que tem comprado no mercado?</p>
-  <a href="#wall-5">Avançar</a>
-  <a href="#wall-3">Voltar</a>
-  </div>
-  <div class="wall wall-5" id="wall-5">
-  <p>Venha avaliar e ajudar a direcionar o dinheir dos consumidores para locais e produtos sustentáveis.</p>
-  <a href="#cadastrar">Vem!</a>
-  </div>
-  </div> 
+  <img src="fotos/mia pose 1-01.png" class="mia" > 
   <form class="cadastro">
     ${Input({ class: 'js-email', placeholder: 'Email', type: 'email' })}
     ${Input({ class: 'js-nome', placeholder: 'Nome completo', type: 'text' })}
     ${Input({ class: 'js-senha', placeholder: 'senha', type: 'password' })}
     ${Button({ title: 'Cadastre-se', onClick: pegarInput })}
+    </form> 
     <p>Você já está registrado? Então venha<a href='#home'> logar</a> e fazer parte de um mundo mais sustentável.</p>
   </div>
   `;
   return template;
 }
 
-/* window.location = "#wall-1"; */
-
-function locationHashChanged() {
-  if (window.location.hash === '#cadastro') {
-    document.querySelector('main').innerHTML = cadastrar();
-  } else if (window.location.hash === '#login') {
-    document.querySelector('main').innerHTML = Login();
-  }
-}
-
-window.addEventListener('hashchange', locationHashChanged, false);
-
 export default cadastrar;
+
+
+/* <div class= 'container-text'>
+  <div  class='item'>
+  <h1>Olá!<br> Eu sou a Mia e vou te contar o porquê precisamos do Base</h1>
+  </div>
+  <a href='#item-2' class='control-btn'>.</a>
+  <div  class='item '>
+  <p>O base, assim como outros aplicativos, utiliza o conceito de crowdsourcing, isso significa que, seus usuários passem as informações criando uma base de dados com avaliações.</p></div>
+  <a href='#item-3' class='control-bt'>.</a>
+  <div  class='item '>
+  <p>Dessa forma, cada um de nós podemos preencher vários questionário de avaliações sobre o nível de sustentabilidade de estabelecimentos comerciais,turísticos e produtos.</p></div>
+  <a href='#item-4' class='control-btn'>.</a>
+  <div  class='item '>
+  <p>Que lugares você tem frequentado?<br>Onde passou as últimas férias?<br>O que tem comprado no mercado?</p>
+  </div>
+  <a href='#item' class='control-btn'><img src="fotos/sairpq.png" alt=""></a>
+  <div  class='item item-5'> 
+  <p>Venha avaliar e ajudar a direcionar o dinheir dos consumidores para locais e produtos sustentáveis.</p>
+  </div>
+    </div>*/
+  
