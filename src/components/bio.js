c56e530bbb8fbbd76ac14ac940764832cdd2c6e0
RachelMarotta <kelmarotta@icloud.com>
2019-10-25 15:38:25 -0300

    correção de bug

--- src/components/bio.js
+++ src/components/bio.js
@@ -1,8 +1,8 @@
 const Bio = (user) => {
   const template = `
-  <div class='users'>
-  <img src='images/user-image.png' class='img-usuario'>
-   <div class='bio'>
+  <div class="users">
+  <img src="images/user-image.png" class="img-usuario">
+   <div class="bio">
     <p>${user.name || 'Nome'}</p>
     <p>${user.age || 'Idade'}</p>
     <p>${user.profession || 'Profissão'}</p>
