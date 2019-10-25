e88e9cc1c68127c181bd10e547542e1ae10b68c1
vanesnery <nery_van@hotmail.com>
2019-10-25 15:07:25 -0300

    arrumando conflito

--- src/main.js
+++ src/main.js
@@ -1,25 +1,24 @@
 import Login from './pages/login.js';
 import Feed from './pages/feed.js';
-import Profile from './pages/profile';
+import Profile from './pages/profile.js';
 import Register from './pages/register.js';
 
 function locationHashChanged() {
-  const dataBase = firebase.firestore();
   firebase.auth().onAuthStateChanged((user) => {
     if (user) {
-      if (location.hash === '#feed') {
-        dataBase.collection('posts')
-          .where('user', '==', user.uid)
+      if (window.location.hash === '#feed') {
+        firebase.firestore().collection('posts')
+          .where('user_uid', '==', user.uid)
           .get()
           .then((querySnapshot) => {
             document.querySelector('main').innerHTML = Feed({
               posts: querySnapshot,
             });
           });
-      } else if (location.hash === '#profile') {
+      } else if (window.location.hash === '#profile') {
         document.querySelector('main').innerHTML = Profile();
       } 
-    } else if (location.hash === '#register') {
+    } else if (window.location.hash === '#register') {
         document.querySelector('main').innerHTML = Register();
       } else {
         document.querySelector('main').innerHTML = Login();
