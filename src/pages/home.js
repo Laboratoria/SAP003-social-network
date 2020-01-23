4491f4a059112f8c0c16bfe574a5772e32eb115f
Fabiana Fonseca <fsf.fabiana8@gmail.com>
2020-01-23 10:31:21 -0300

    ajustando o css

--- src/pages/home.js
+++ src/pages/home.js
@@ -9,18 +9,20 @@ function Home() {
     <h1 class="text-center teste">ALCATEIA</h1>
   </div>
     <div class="banner"></div>
-  <h2> Faça login na sua conta </h2>
-  <form>
-  ${Input({placeholder: 'Email', type: 'email', class: 'js-email-input'})}
-  ${Input({placeholder: 'Senha', type: 'password', class: 'js-password-input'})}
-  ${Button({id: 'button',  title: 'Entrar', onClick: buttonLogin, class:'bntentrar'})}<br>
-  
-  ${Button({id:'google-btn', onClick:googleLogin, class:'bntgoogle' , title: '<img src= img/google.png width="60" height="70"> '})}
-  
-  </form><br>
-  
-  <p class="register-link">Não é cadastrado? <a class="register-hash" href="#register">Registre-se.</a></p>
-  `
+
+  <div class="box"> 
+      <h2> Faça login na sua conta </h2>
+      <form>
+      ${Input({placeholder: 'Email', type: 'email', class: 'js-email-input'})}
+      ${Input({placeholder: 'Senha', type: 'password', class: 'js-password-input'})}
+      ${Button({id: 'button',  title: 'Entrar', onClick: buttonLogin, class:'bntentrar'})}<br>
+      
+      ${Button({id:'google-btn', onClick:googleLogin, class:'bntgoogle' , title: '<img src= img/google.png width="60" height="70"> '})}
+      
+      </form><br>
+      
+      <p class="register-link">Não é cadastrado? <a class="register-hash" href="#register">Registre-se.</a></p>
+  </div>`
     return template;
 };
 export default Home;
